36e4c146dc74feebada1bfa1c01cc821e79d27b0
yu-xinh7 <yuxinh2@nycstudents.net>
2024-10-24 10:27:42 -0400

    Update script.js

--- script.js
+++ script.js
@@ -58,3 +58,22 @@ console.log(checkWinner);
 blueButton.addEventListener("click", advanceBlue);
 pinkButton.addEventListener("click", advancePink);
 brownButton.addEventListener("click", advanceBrown);
+
+// call it in each of the advanceColor() functions
+const advanceBlue = (e) => {
+  blue_position += 1;
+  changePosition(blueHorse, blue_position);
+  checkWinner(blue_position, "Blue");
+};
+
+const advancePink = (e) => {
+  pink_position += 1;
+  changePosition(pinkHorse, pink_position);
+  checkWinner(pink_position, "Pink");
+};
+
+const advanceBrown = (e) => {
+  pink_position += 1;
+  changePosition(brownHorse, brown_position);
+  checkWinner(brown_position, "Brown");
+};
